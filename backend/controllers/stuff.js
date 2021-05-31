dd8c8d7dbdb4bb5dc2d9e123eb30cf38c05f1509
Ln <rainbowliketravel@gmail.com>
2021-05-31 16:34:45 +0200

    correction switch case 1 et case -1

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -52,10 +52,10 @@ exports.likeOrDislike = (req, res, next) => {
       .then (sauce => {
         switch (req.body.like) {
           case 1: 
-            if (!sauce.userLiked.includes(req.body.userId)) {
+            if (!sauce.usersLiked.includes(req.body.userId)) {
               Sauce.updateOne({ _id: req.params.id }, 
                               { $inc: { likes: 1 }, 
-                                $push: { userliked:req.body.userId },
+                                $push: { usersliked:req.body.userId },
                                 _id: req.params.id })                            
               .then(()=> res.status(201).json({ message : 'like ajouté avec succès' }))
               .catch(error => res.status(404).json({ error })
@@ -63,27 +63,27 @@ exports.likeOrDislike = (req, res, next) => {
             }
             break;
           case 0: 
-          if (!sauce.userLiked.includes(req.body.userId)) {
+          if (!sauce.usersLiked.includes(req.body.userId)) {
             Sauce.updateOne({ _id: req.params.id }, 
               { $inc: { likes, dislikes: 0 },
-                $push: { userliked, userDisliked } })                          
+                $push: { usersliked, usersDisliked } })                          
             .then(()=> res.status(201).json({ message : 'modification effectuée' }))
             .catch(error => res.status(404).json({ error })
             )
           } else {
             Sauce.updateOne({ _id: req.params.id }, 
               { $inc: { likes, dislikes: 0 },
-                $push: { userliked, userDisliked } })                           
+                $push: { usersliked, usersDisliked } })                           
             .then(()=> res.status(201).json({ message : 'modification effectuée' }))
             .catch(error => res.status(404).json({ error })
             )
           }
             break;
           case -1: 
-          if (!sauce.userDisliked.includes(req.body.userId)) {
+          if (!sauce.usersDisliked.includes(req.body.userId)) {
             Sauce.updateOne({ _id: req.params.id }, 
                             { $inc: { likes: -1 }, 
-                              $push: { userDisliked:req.body.userId },
+                              $push: { usersDisliked:req.body.userId },
                               _id: req.params.id })                            
             .then(()=> res.status(201).json({ message : 'like supprimé avec succès' }))
             .catch(error => res.status(404).json({ error })
